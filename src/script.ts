a7236c1f420a4a292ced890ea5205810787703fb
MelinaSlhk <melinaloony@gmail.com>
2023-06-29 16:42:21 +0200

    ajout du get/set

--- src/script.ts
+++ src/script.ts
@@ -3,7 +3,7 @@ class Hero {
     private power:number;
     private life:number;
 
-    constructor(name:string,power:number,life:number) {
+    constructor(name:string, power:number, life:number) {
       
         this.name = name;
         this.power = power;
@@ -16,9 +16,31 @@ class Hero {
     }
     
     isAlive(){
-        this.life > 0;
+       return this.life > 0;
     }
 
+    getname(): string {
+        return this.name
+    }
+
+    setname(nouveauNom : string) {
+        this.name = nouveauNom;
+    }
+    
+    getpower(): number {
+        return this.power
+    }
+
+    setpower(nouveauPower : string) {
+        this.name = nouveauPower;
+    }
+    
+    getlife(): number {
+        return this.life
+    }
+    setlife(nouvelleLife : number) {
+        this.life = nouvelleLife;
+    }
 }
 
 let hercule:Hero = new Hero("hercule", 19, 300)
@@ -27,3 +49,5 @@ let johnsson:Hero = new Hero("johnsson", 19, 200)
  
 hercule.attack(johnsson)
 johnsson.attack(hercule)
+
+}
